2571ffdefe0a257cc1ebe590639cb0395bd84013
LorKamiT <lorkamimau@gmail.com>
2024-09-07 03:18:31 -0600

    mas avance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-import Image from "next/image";
-import { NextUIProvider, User, Slider } from "@nextui-org/react";
+import {
+  NextUIProvider,
+  User,
+  Slider,
+  Card,
+  CardFooter,
+  Image,
+  Button,
+} from "@nextui-org/react";
 
 export default function Home() {
   return (
@@ -20,8 +27,7 @@ export default function Home() {
           <div className="NavBTN flex justify-end items-center w-full md:w-2/3 xl:w-1/2 2xl:w-1/4 h-24">
             <div className="NavBox flex flex-row justify-around w-full h-full">
               <a
-                href="https://www.youtube.com/watch?v=GAcq61I9IVM"
-                target="_blank"
+                href="/about"
                 className=" hover:scale-90 hover:animate-squeeze hover:animate-iteration-count-infinite flex justify-center"
               >
                 <Image
@@ -29,7 +35,7 @@ export default function Home() {
                   width={60}
                   height={60}
                   className="object-contain"
-                  alt="Menu"
+                  alt="About"
                 />
               </a>
               <a
@@ -86,24 +92,84 @@ export default function Home() {
             height={500}
             alt="Logo"
           />
-          <Slider
-            label="Temperature"
-            step={0.01}
-            maxValue={1}
-            minValue={0}
-            defaultValue={0.4}
-            className="max-w-md"
-          />
         </div>
-        <div className="SegundaSeccion flex flex-col justify-center items-center gap-8 w-full">
+      </div>
+      <div className="SegundaSeccion flex flex-col justify-center items-center gap-8 w-full h-svh bg-[url('/images/LogoGif.gif')] bg-no-repeat bg-fixed bg-center">
+        <div className="AvatarSec flex flex-col md:flex-row gap-3 justify-around items-center bg-neutral-900/80 rounded-xl w-auto md:w-[50%] h-auto md:h-24 p-5">
           <User
-            name="Mauricio D."
-            description="Product Designer"
+            name="LorKami Dev"
+            description="Noob Dev"
             avatarProps={{
-              src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+              src: "/images/LogoLK.png",
+            }}
+          />
+          <User
+            name="LorKami Dev"
+            description="Noob Dev"
+            avatarProps={{
+              src: "/images/LogoLK.png",
+            }}
+          />
+          <User
+            name="LorKami Dev"
+            description="Noob Dev"
+            avatarProps={{
+              src: "/images/LogoLK.png",
             }}
           />
         </div>
+        <div className="TarjetaFoto flex flex-row justify-around w-full h-auto p-8">
+          <Card isFooterBlurred radius="lg" className="border-none">
+            <Image
+              alt="Kami"
+              className="object-cover"
+              height={250}
+              src="/images/PUMP_IT.png"
+              width={200}
+            />
+            <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+              <p className="text-tiny text-white/80">Soy nuv</p>
+              <Button
+                className="text-tiny text-white bg-black/20"
+                variant="flat"
+                color="default"
+                radius="lg"
+                size="sm"
+              >
+                Ya lo sabemos
+              </Button>
+            </CardFooter>
+          </Card>
+          <Card isFooterBlurred radius="lg" className="border-none">
+            <Image
+              alt="Kami"
+              className="object-cover"
+              height={250}
+              src="/images/LogoLK.png"
+              width={200}
+            />
+            <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+              <p className="text-tiny text-white/80">Soy nuv x2</p>
+              <Button
+                className="text-tiny text-white bg-black/20"
+                variant="flat"
+                color="default"
+                radius="lg"
+                size="sm"
+              >
+                Ya lo sabemos x3
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+        <Slider
+          label="Nivel de epicidad"
+          step={0.01}
+          maxValue={1}
+          minValue={0}
+          defaultValue={0.5}
+          className="max-w-md"
+        />
       </div>
     </NextUIProvider>
   );
